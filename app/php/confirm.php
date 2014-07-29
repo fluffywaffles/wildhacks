48911eaa0870b268cd9708bcd014eb1ba900e35c
Michael Chen <MikeTYChen@Michaels-MBP.att.net>
2014-07-29 14:10:55 -0500

    php email confirm

---
+++ app/php/confirm.php
@@ -0,0 +1,15 @@
+<?php
+$field_name = $_POST['name'];
+$field_email = $_POST['email'];
+$field_message = $_POST['comments'];
+
+$subject = 'WildHacks Application Received!';
+
+$body_message = "Hey ".$field_name.",\n\n";
+$body_message .= "Thanks for applying! Sometime in the next few weeks you'll receive a signup link for WildHacks. \n\n";
+$body_message .= "We can't wait for November, and we hope you feel the same way!\n\nIn the meantime, please don't hesistate to reach out to us with any questions at team@wildhacks.org \n\n";
+$body_message .="Best, \nYour WildHacks Team";
+
+$headers = "From: Team@wildhacks.org\r\n";
+$headers .= "Reply-To: Team@wildhacks.org\r\n";
+$mail_status = mail($field_email, $subject, $body_message, $headers);
