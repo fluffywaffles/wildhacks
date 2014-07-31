89fd4e68495b17231bbb4e9ee091ee59d1a18bb9
skorlir <skorlir@gmail.com>
2014-07-31 00:47:08 -0500

    wip form submission email js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,3 +4,10 @@ $(document).ready(function() {
     $(this).toggleClass('active');
   });
 });
+
+function submitRegistration () {
+  var name  = document.forms['ss-form']['entry.145219669'].value,
+      email = document.forms['ss-form']['entry.2028281063'].value;
+  
+  $.post('http://www.wildhacks.org/server/confirm.php', {name: name, email: email});
+}
